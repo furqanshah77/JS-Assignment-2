e12b05502dd9ca985eadf2d382c7924fd90e53a5
Furqanshah77 <shahfurqan668@gmail.com>
2023-10-21 22:55:59 +0500

    Ready-to-submit

--- app.js
+++ app.js
@@ -69,26 +69,26 @@
 
 // alert("Totle Cost To Buy" + " " + ticket + " " + "Tickets" + " " + "To A Movie Is" + " " + totle + " " + "PKR");
 
-// document.write("Totle Cost To Buy" + " " + ticket + " " + "Tickets" + " " + "To A Movie Is" + " " + totle + " " + "PKR"); 
+// document.write("Totle Cost To Buy" + " " + ticket + " " + "Tickets" + " " + "To A Movie Is" + " " + totle + " " + "PKR");
 
 
 
 
-// Question No # (4)
+// // Question No # (4)
 
-function generateTable() {
+// function generateTable() {
 
-    var number = prompt("Enter a number:");
-    var tableString = "Your Value is" + " " + number + ":<br>";
+//     var number = prompt("Enter a number:");
+//     var tableString = "Your Value is" + " " + number + ":<br>";
 
-    document.write(tableString);
-    for (var i = 1; i <= 10; i++) {
-        document.write(+ number + " x " + i + "=" + (number * i) + "<br>");
-    }
+//     document.write(tableString);
+//     for (var i = 1; i <= 10; i++) {
+//         document.write(+ number + " x " + i + "=" + (number * i) + "<br>");
+//     }
 
-}
+// }
 
-generateTable();
+// generateTable();
 
 
 
@@ -112,3 +112,96 @@ generateTable();
 // var celsiusTemperature = (fahrenheitTemperature - 32) * 5 / 9;
 
 // document.write(fahrenheitTemperature + "°F is " + celsiusTemperature + "°C");
+
+
+
+
+// // Question No # (6)
+
+// var totalMarks = +prompt("Enter Your Total Marks");
+
+// var obtainedMarks = +prompt("Enter Your Obtained Marks");
+
+// var percentage = (obtainedMarks / totalMarks) * 100;
+
+// var grade = ""
+
+// document.write('Total Marks :' + " " + totalMarks + "<br>");
+
+// document.write("Marks Obtained :" + " " + obtainedMarks + "<br>");
+
+// document.write("Percentage :" + " " + Math.round(percentage) + "%" + "<br>");
+
+// if (percentage <= 40) {
+//     grade = "Failed"
+// } else if (percentage >= 90) {
+//     grade = "A+"
+// } else if (percentage >= 80) {
+//     grade = "A"
+// } else if (percentage >= 70) {
+//     grade = "B"
+// } else if (percentage >= 60) {
+//     grade = "C"
+// } else if (percentage >= 50) {
+//     grade = "E"
+// } else if (percentage >= 40) {
+//     grade = "D"
+// };
+
+// document.write("Grade :" + " " + grade + "<br>");
+
+
+
+// // Question No # (7)
+
+// var usDEollar = +prompt("How Many Dollar You Have?");
+
+// var riyal = +prompt("How Many Riyal You Have?");
+
+// var pkr = (usDEollar * 275.00) + (riyal * 73.31);
+
+// document.write("Total Currency In PKR Is" + " " + "=" + " " + pkr + "<br>")
+
+
+
+
+// // Question No # (7)
+
+// var price1 = 650;
+
+// var item1 = +prompt("Enter item1 your quatity");
+
+// var price2 = 100;
+
+// var item2 = +prompt("Enter your Items 2 quantity");
+
+// var deliveryCharges = 100;
+
+// var totalCost = (price1 * item1) +(price2 * item2) + deliveryCharges;
+
+// document.write("Price Of Item 1 is " + " " + price1 + "<br>");
+
+// document.write('Quantity Of Item 1 is'+" " + item1 + '<br>');
+
+// document.write('Price Of Item 2 is' +" " + price2 + "<br>");
+
+// document.write("Quantity Of Item 2 is" + " " + item2 + "<br>");
+
+// document.write("Shipping Charges" + deliveryCharges + "<br>");
+
+// document.write("Total Cost Of Your Order is" + " " + totalCost + "<br>")
+
+
+
+
+// // Question No # (7)
+
+// var birthYear = +prompt("Enter Your Birth Year");
+
+// var date = new Date();
+
+// var year = date.getFullYear();
+
+// var age = year - birthYear ;
+
+// document.write("Your Age is " + " "+ age + "<br>" )
